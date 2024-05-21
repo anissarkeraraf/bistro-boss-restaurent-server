82b9a7e554da47ceaeca09a55f3fe638ce904135
anissarkeraraf <anissarkeraraf@gmail.com>
2024-05-21 10:48:04 +0600

    added the carts with get

--- index.js
+++ index.js
@@ -49,6 +49,13 @@ async function run() {
       res.send(result);
     })
 
+    app.get('/carts', async(req, res) => {
+      const email = req.query.email;
+      const query = {email: email};
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    })
+
 
 
     // Send a ping to confirm a successful connection
